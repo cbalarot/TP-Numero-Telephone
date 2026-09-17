31df3b16024361938f815ad107d22fdbff520f9f
cbalarot <clement.balarot@lyceeastier.com>
2026-09-17 11:15:53 +0200

    init

---
+++ src/Ihm.java
@@ -0,0 +1,94 @@
+import clavier.In;
+
+public class Ihm {
+    public static void main(String[] args) {
+        String telephone, region="", zone = "";
+        String telP1, telP2;
+        boolean mobile = false;
+
+        //Input
+        System.out.print("Entrez votre numéro de téléphone : ");
+        telephone = In.readString();
+
+        //Verif input
+        if (telephone.length() != 10) {
+            System.out.println("Entrez un numéro de téléphone valide !");
+            return;
+        }
+
+        telP1 = telephone.substring(0, 2);
+        telP2 = telephone.substring(2, 4);
+
+        switch (telP1) {
+            case "01":
+                region = new String("parisienne");
+                switch (telP2) {
+                    case "34", "39":
+                        zone = new String("Yvelines");
+                        break;
+                    case "40":
+                        zone = new String("Paris");
+                        break;
+                    default:
+                        zone = new String("Non géré");
+                }
+                break;
+            case "02":
+                region = new String("Nord Ouest");
+                break;
+            case "03":
+                region = new String("Nord Est");
+                break;
+            case "04":
+                region = new String("Sud Est");
+                switch (telP2) {
+                    case "75":
+                        zone = new String("Drôme/Ardèche");
+                        break;
+                    default:
+                        zone = new String("Non géré");
+                }
+                break;
+            case "05":
+                region = new String("Sud Ouest");
+                break;
+            case "06":
+                mobile = true;
+                switch (telP2) {
+                    case "01":
+                        zone = new String("Neuf Cegetel");
+                        break;
+                    case "03", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
+                         "24", "25", "26", "27", "28", "29":
+                        zone = new String("SFR");
+                        break;
+                    case "05":
+                        zone = new String("Auchan Télécom");
+                        break;
+                    case "07", "08", "30", "31", "32", "33", "34", "35", "36":
+                        zone = new String("Orange");
+                        break;
+                }
+                break;
+            case "07":
+                mobile = true;
+                zone = new String("");
+                break;
+            case "09":
+                break;
+            default:
+                System.out.println("\rVotre numéro de téléphone n'est pas surpporté par ce logiciel");
+                return;
+        }
+
+        System.out.print("Votre numéro " + telephone);
+        if (mobile) {
+            System.out.print(" est un numéro mobile");
+            if (!zone.isEmpty()) {
+                System.out.println(" de l'opérateur " + zone);
+            }
+        } else {
+            System.out.println(" est localisé en region " + region + " dans la zone " + zone);
+        }
+    }
+}
